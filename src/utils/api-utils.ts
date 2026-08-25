0f0863595d9a6b0223e33d5acdd61da95e7984d2
Aleksandr <118198548+sundstorm91@users.noreply.github.com>
2026-08-25 21:32:00 +0700

    chore: update BASE_URL for deploy

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,6 +1,6 @@
 import { User } from '../services/features/user/types';
 
-const BASE_URL = 'https://01a00fca-b989-7cb1-bb13-b0f558a3e4f6.tunnel4.com';
+const BASE_URL = 'https://a2552e51-cf26-4515-8009-2d6581b90d65.tunnel4.com';
 
 export const ingredientsApiConfig = {
 	baseUrl: BASE_URL,
